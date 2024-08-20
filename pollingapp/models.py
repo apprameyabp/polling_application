ed1d18ba8ae284c0b70309cb1fc98c5c657aea89
apprameyabp <apprameyabp106@gmail.com>
2024-08-20 16:45:17 +0530

    migrated the model:created db and necessary tables

--- pollingapp/models.py
+++ pollingapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    
+    class Meta:
+        db_table = 'Question'
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.TextField()
+    votes = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'Choice'
+
+        
+
+
